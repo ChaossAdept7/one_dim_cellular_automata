b7a25f7d448b3abbfbcb990164c72e8920baf9a9
Serj Nefodov <chaossofmailhere@gmail.com>
2023-05-14 00:58:17 +0300

    remove un-necessary

--- Readme.md
+++ Readme.md
@@ -1,2 +1,6 @@
 Code above is written by chat GPT, for illustration one dimensional cellular automata. 
-Change 
+How to run: `python main.py`
+
+Note that you need to have a python 3.7 to run this.
+
+Or, you can run it using site https://ide.geeksforgeeks.org/online-python-compiler
--- main.py
+++ main.py
@@ -1,6 +1,27 @@
-from cellular_automata import cellular_automaton
+def cellular_automaton(rule_number, initial_state, steps):
+    # Obtaining a binary representation of a rule
+    rule_string = format(rule_number, '08b')
+    rule = list(map(int, rule_string))
 
-rule = 90
+    # Initializing the current state
+    current_state = initial_state
+
+    for _ in range(steps):
+        print(''.join(current_state).replace('0', ' ').replace('1', '*'))
+        new_state = ''
+
+        for i in range(len(current_state)):
+            # Determining the neighborhood of each cell
+            left = '0' if i == 0 else current_state[i - 1]
+            center = current_state[i]
+            right = '0' if i == len(current_state) - 1 else current_state[i + 1]
+            neighborhood = left + center + right
+
+            # Applying a Rule to a Neighborhood
+            rule_index = 7 - int(neighborhood, 2)
+            new_state += str(rule[rule_index])
+
+        current_state = new_state
 
 # other rules.
 # 222 == 11011110
@@ -10,8 +31,9 @@ rule = 90
 # 90 == 01011010
 # 259 == 100000011
 
+rule_number = 90
 begin_generation = '0000000000000000000100000000000000000000'
 number_of_generations = 20
 
 
-cellular_automaton(rule, begin_generation, number_of_generations)
+cellular_automaton(rule_number, begin_generation, number_of_generations)
